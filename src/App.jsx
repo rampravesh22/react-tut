9e51d76f2e8645f3b7bdcf085e66c809c585fc42
rpc <rpc7863@gamil.com>
2022-11-27 07:26:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
-	return <div className="app"></div>;
+	const [show, setShow] = useState(true);
+	const [hide, setHide] = useState(false);
+	const [students, setStudents] = useState([]);
+	const [loadin, setLoadin] = useState(false);
+
+	const handleShow = () => {
+		setLoadin(true);
+		setTimeout(() => {
+			const getData = async () => {
+				try {
+					const response = await axios.get(
+						"http://localhost:8000/student/"
+					);
+					setStudents(response.data.results);
+					console.log(response.data.results);
+					setLoadin(false);
+					setShow(false);
+					setHide(true);
+				} catch (error) {}
+			};
+			getData();
+		}, 4000);
+	};
+
+	const handleHide = () => {
+		setStudents([]);
+		setShow(true);
+		setHide(false);
+	};
+
+	return (
+		<div className="app">
+			{show ? <button onClick={handleShow}>Show Students</button> : null}
+			{hide ? <button onClick={handleHide}>Hide Students</button> : null}
+			{loadin ? <h1>Loading Data...</h1> : null}
+			<ul>
+				{students.map((student) => {
+					return <li key={student.id}>{student.name}</li>;
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
