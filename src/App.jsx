8e935bcebcf9de5b73595cdb2bb2497d1066545b
rpc1752 <rpc1752@gamil.com>
2022-11-29 14:48:17 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Register from "./Register";
+
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<Register></Register>
+		</div>
+	);
 }
 
 export default App;
