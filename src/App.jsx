3d42982054836a2f27b1457f041e06bb47e572d6
rpc1752 <rpc1752@gamil.com>
2022-12-03 12:33:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useReducer, useState } from "react";
+const reducer = (state, action) => {
+	console.log(action);
+	switch (action.type) {
+		case "ADD":
+			return state + 1;
+		case "REMOVE":
+			return state - 1;
+		default:
+			return state;
+	}
+};
 function App() {
-	return <div className="app"></div>;
+	const [state, disptach] = useReducer(reducer, 0);
+	return (
+		<div className="app">
+			<button onClick={() => disptach({ type: "ADD" })}>Add</button>
+			<button onClick={() => disptach({ type: "REMOVE" })}>REMOVE</button>
+			<br />
+			<br />
+			{state}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/store/reducer.jsx
@@ -0,0 +1,21 @@
+// const id = 0;
+// const reducer = (state = [], action) => {
+// 	switch (action.type) {
+// 		case "ADD_TASK":
+// 			return [
+// 				...state,
+// 				{
+// 					id: id + 1,
+// 					task: action.payload.task,
+// 					completed: false,
+// 				},
+// 			];
+// 		case "REMOVE_TASK":
+// 			return state.filter((task) => task.id !== action.payload.id);
+// 		case "COMPLETE_TASK":
+// 			break;
+// 		default:
+// 			state;
+// 	}
+// };
+// export default reducer;
