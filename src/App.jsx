88dace5ad65f0eb4eb261d808e8e43ddb38316bf
rpc <rpc7863@gamil.com>
2022-12-03 20:54:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-	return <div className="app"></div>;
+	const [count, setCount] = useState(1);
+	useEffect(() => {
+		console.log("useEffect called", count);
+	}, []);
+	return (
+		<>
+			<div
+				style={{
+					margin: "4rem",
+					fontSize: "4rem",
+				}}
+			>
+				{count}
+			</div>
+			<button onClick={() => setCount(count + 1)}>Increase</button>
+		</>
+	);
 }
 
 export default App;
