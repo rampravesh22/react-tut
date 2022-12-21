bf3960609ba3870273929f39cf583aa0a649f911
rpc <rpc7863@gamil.com>
2022-12-21 06:25:01 +0530

    get all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useGetAllPostQuery } from "./services/post";
+
 function App() {
-	return <div className="app"></div>;
+	const { data, isError, isSuccess, isLoading } = useGetAllPostQuery();
+	console.log(data);
+	return (
+		<div className="app">
+			<div>{isLoading ? <h2>Loding...</h2> : null}</div>
+			<div>{isError ? <h2>Error</h2> : null}</div>
+			{data
+				? data.map((post) => {
+						return <li key={post.id}>{post.title}</li>;
+				  })
+				: null}
+		</div>
+	);
 }
 
 export default App;
