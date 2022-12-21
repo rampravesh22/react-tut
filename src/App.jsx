0c33846417a741a8616b5d312248d1489b104e0c
rpc <rpc7863@gamil.com>
2022-12-21 06:39:34 +0530

    get single data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useGetAllPostQuery } from "./services/post";
+import { useGetAllPostQuery, useGetPostByIdQuery } from "./services/post";
 
 function App() {
-	const { data, isError, isSuccess, isLoading } = useGetAllPostQuery();
-	console.log(data);
+	const { data, isError, isSuccess, isLoading } = useGetPostByIdQuery(1);
 	return (
 		<div className="app">
 			<div>{isLoading ? <h2>Loding...</h2> : null}</div>
 			<div>{isError ? <h2>Error</h2> : null}</div>
-			{data
-				? data.map((post) => {
-						return <li key={post.id}>{post.title}</li>;
-				  })
-				: null}
+
+			<div className="data-by-id">{data.title}</div>
 		</div>
 	);
 }
